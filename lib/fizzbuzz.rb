47244f40943d99fa6f32c48f0526c098b1ed7a40
shafali03 <shaf03sm@gmail.com>
2019-10-31 20:05:54 +0000

    restarted, passing for 2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,23 +1,3 @@
 def fizzbuzz(number)
-  if fizz(number) and buzz(number)
-    'fizzbuzz'
-  elsif
-    buzz(number)
-    'buzz'
-  elsif
-    fizz(number)
-      'fizz'
-  else
-  number
-  end
-end
-
-
-
-def fizz(number)
- number % 3 == 0
-end
-
-def buzz(number)
-  number % 5 == 0
+  2
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,24 +1,10 @@
-require './lib/fizzbuzz'
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
+  it 'return 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
+  end
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
-  end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
-  end
-  it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
-  end
-  it 'returns "2" when passed 2' do
-    expect(fizzbuzz(2)).to eq 2
-  end#
-  it 'returns "fizz" when passed 6' do
-    expect(fizzbuzz(6)).to eq 'fizz'
-  end
-  it 'returns "buzz" when passed 10' do
-    expect(fizzbuzz(10)).to eq 'buzz'
-  end
-  it 'returns "fizzbuzz" when passed 30' do
-    expect(fizzbuzz(30)).to eq 'fizzbuzz'
   end
 end
